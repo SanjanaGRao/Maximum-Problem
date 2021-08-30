3ca49df1ad98aca110c18f672e9d11f60f711f7b
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-30 20:10:13 +0530

    Added: Extend the max

--- TestCases.java
+++ TestCases.java
@@ -1,16 +1,45 @@
+import java.util.Scanner;
 
 public class TestCases {
 	
 	public static void main(String[] args)
 	{
-		int firstInt=30, secondInt=55,thirdInt=21;
-		new TestMaximum(firstInt,secondInt,thirdInt).maximumValue(firstInt,secondInt,thirdInt);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of parameters you want (btw 3 - 5): ");
+		int option = sc.nextInt();
 		
-		double firstFloat=300.75, secondFloat=300.25,thirdFloat=300.95;
-		new TestMaximum(firstFloat,secondFloat,thirdFloat).maximumValue(firstFloat,secondFloat,thirdFloat);
-		
-		String firstWord= "Apple", secondWord= "Peach",thirdWord= "Banana";
-		new TestMaximum(firstWord,secondWord,thirdWord).maximumValue(firstWord,secondWord,thirdWord);
+		switch(option)
+		{
+		case 3: {
+			int firstInt=30, secondInt=55,thirdInt=21;
+			new TestMaximum(firstInt,secondInt,thirdInt).maximumValue(firstInt,secondInt,thirdInt);
+			double firstFloat=300.75, secondFloat=300.25,thirdFloat=300.95;
+			new TestMaximum(firstFloat,secondFloat,thirdFloat).maximumValue(firstFloat,secondFloat,thirdFloat);		
+			String firstWord= "Apple", secondWord= "Peach",thirdWord= "Banana";
+			new TestMaximum(firstWord,secondWord,thirdWord).maximumValue(firstWord,secondWord,thirdWord);
+			break;
+		}
+		case 4: {
+			int firstInt=30, secondInt=55,thirdInt=21, fourthInt=45;
+			new TestMaximum(firstInt,secondInt,thirdInt,fourthInt).maximumValue(firstInt,secondInt,thirdInt,fourthInt);
+			double firstFloat=300.75, secondFloat=300.25,thirdFloat=300.95, fourthFloat=300.00;
+			new TestMaximum(firstFloat,secondFloat,thirdFloat,fourthFloat).maximumValue(firstFloat,secondFloat,thirdFloat,fourthFloat);		
+			String firstWord= "Apple", secondWord= "Peach",thirdWord= "Banana", fourthWord="Strawberry";
+			new TestMaximum(firstWord,secondWord,thirdWord,fourthWord).maximumValue(firstWord,secondWord,thirdWord,fourthWord);
+			break;
+		}
+		case 5: {
+			int firstInt=30, secondInt=55,thirdInt=21, fourthInt=45, fifthInt=3;
+			new TestMaximum(firstInt,secondInt,thirdInt,fourthInt,fifthInt).maximumValue(firstInt,secondInt,thirdInt,fourthInt,fifthInt);
+			double firstFloat=300.75, secondFloat=300.25,thirdFloat=300.95, fourthFloat=300.00, fifthFloat= 407;
+			new TestMaximum(firstFloat,secondFloat,thirdFloat,fourthFloat,fifthFloat).maximumValue(firstFloat,secondFloat,thirdFloat,fourthFloat,fifthFloat);		
+			String firstWord= "Apple", secondWord= "Peach",thirdWord= "Banana", fourthWord="Strawberry", fifthWord = "Pear";
+			new TestMaximum(firstWord,secondWord,thirdWord,fourthWord,fifthWord).maximumValue(firstWord,secondWord,thirdWord,fourthWord,fifthWord);
+			break;
+		}
+		default: System.out.println("Invalid option.");
+		}// end of switch case	
+		sc.close();
 	}
 
 }
--- TestMaximum.java
+++ TestMaximum.java
@@ -1,15 +1,31 @@
 
 public class TestMaximum<T extends Comparable<T>> {
 	
-	T first,second,third;
+	T first,second,third,fourth,fifth;
 	
-	//Constructor
+	//Constructor when user wants 3 parameters
 	public TestMaximum(T first,T second,T third) {
 		this.first=first;
 		this.second=second;
 		this.third=third;
 	}
 	
+	//Constructor when user wants 4 parameters
+	public TestMaximum(T first,T second,T third,T fourth) {
+			this.first=first;
+			this.second=second;
+			this.third=third;
+			this.fourth=fourth;
+		}
+	
+	//Constructor when user wants 3 parameter
+	public TestMaximum(T first,T second,T third,T fourth,T fifth) {
+			this.first=first;
+			this.second=second;
+			this.third=third;
+			this.fourth=fourth;
+			this.fifth=fifth;
+		}
 	/*Generic method using compareTo to find out the maximum of Integers
 	 * @param max is used to assume first is maximum then compare with second and third
 	 * Refactoring: To create Generic Class to take in 3 variables of Generic Type
@@ -26,10 +42,61 @@ public class TestMaximum<T extends Comparable<T>> {
 			return max;
 		}
 	 
-	 //Method to print the maximum Value
+	 /*Generic method using compareTo to find out the maximum of Integers
+		 * @param max is used to assume first is maximum then compare with second, third and fourth
+		 * Refactoring: To create Generic Class to take in 4 variables of Generic Type
+		 */
+	public static <T extends Comparable<T>> T maximumValue(T first,T second,T third,T fourth) {
+				T max = first;  
+				if(second.compareTo(max)>0) {
+					max=second;
+				}
+				if(third.compareTo(max)>0) {
+					max=third;
+				}
+				if(fourth.compareTo(max)>0) {
+					max=fourth;
+				}
+				printMax(first,second,third,fourth,max);
+				return max;
+			}
+	
+	 /*Generic method using compareTo to find out the maximum of Integers
+	 * @param max is used to assume first is maximum then compare with second, third and fourth
+	 * Refactoring: To create Generic Class to take in 4 variables of Generic Type
+	 */
+	public static <T extends Comparable<T>> T maximumValue(T first,T second,T third,T fourth,T fifth) {
+				T max = first;  
+				if(second.compareTo(max)>0) {
+					max=second;
+				}
+				if(third.compareTo(max)>0) {
+					max=third;
+				}
+				if(fourth.compareTo(max)>0) {
+					max=fourth;
+				}
+				if(fifth.compareTo(max)>0) {
+					max=fifth;
+				}
+				printMax(first,second,third,fourth,fifth,max);
+				return max;
+			}
+	 
+	 //Method to print the maximum Value for 3 parameters.
 	 public static <T> void printMax(T first,T second,T third,T max)
 		{
 			System.out.printf("Maximum of %s, %s and %s is %s .\n",first,second,third,max);
 		}
+	//Method to print the maximum Value for 4 parameters.
+	 public static <T> void printMax(T first,T second,T third,T fourth,T max)
+			{
+				System.out.printf("Maximum of %s, %s, %s and %s is %s .\n",first,second,third,fourth,max);
+			}
+	//Method to print the maximum Value for 5 parameters.
+	public static <T> void printMax(T first,T second,T third,T fourth,T fifth,T max)
+			{
+				System.out.printf("Maximum of %s, %s, %s, %s and %s is %s .\n",first,second,third,fourth,fifth,max);
+			}
 
 }
